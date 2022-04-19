bd3eb157a4f1a6acc495b6b274939b3bca347a30
Paqimm <mcgeewilliam815@gmail.com>
2022-04-19 11:59:56 +0300

    chore (Project) : setup project

---
+++ studytimeapi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for studytimeapi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studytimeapi.settings')
+
+application = get_asgi_application()
