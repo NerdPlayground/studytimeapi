38c42f19b4c72a423f3537eadaf411f5155cf442
Paqimm <mcgeewilliam815@gmail.com>
2022-04-19 15:07:38 +0300

    fix (Endpoints) : page not found

--- studytimeapi/urls.py
+++ studytimeapi/urls.py
@@ -38,5 +38,6 @@ urlpatterns = [
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('',include("members.urls")),
+    path('',include("topics.urls")),
     # path('',include(".urls")),
 ]
