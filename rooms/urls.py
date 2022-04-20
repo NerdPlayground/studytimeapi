a1e0524c1151faeb3f6924790591ef6cc9131d31
Paqimm <mcgeewilliam815@gmail.com>
2022-04-20 16:41:40 +0300

    fix (Naming) : differentiate url paths

--- rooms/urls.py
+++ rooms/urls.py
@@ -8,6 +8,6 @@ urlpatterns= [
     path('add-room/',AddRoomAPIView.as_view(),name="add-room"),
     path('all-rooms/',AllRoomsAPIView.as_view(),name="all-rooms"),
     path('my-rooms/',MemberRoomsAPIView.as_view(),name="my-rooms"),
-    path('room/<str:pk>/',IndividualRoomAPIView.as_view(),name="individual-room"),
+    path('individual-room/<str:pk>/',IndividualRoomAPIView.as_view(),name="individual-room"),
     path('room/<str:pk>/',RoomDetailAPIView.as_view(),name="room-detail")
 ]
