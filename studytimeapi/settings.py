101641babc2c0ea6f53f3d5c337b4592efd461e5
Paqimm <mcgeewilliam815@gmail.com>
2022-07-27 12:08:23 +0300

    refractor (Database) : use PostgreSQL

--- studytimeapi/settings.py
+++ studytimeapi/settings.py
@@ -87,11 +87,14 @@ WSGI_APPLICATION = 'studytimeapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbpostgres',
+        'USER': 'postgres',
+        'PASSWORD': 'PostgreSQL23!@#',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
